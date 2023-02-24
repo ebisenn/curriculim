d7316f73a9847040aa276654ee5e76d825e399df
ebisenn <123216301+ebisenn@users.noreply.github.com>
2023-02-25 08:31:20 +0900

    練習問題1（再）
    練習問題1（再提出）

--- PHPjob/3-2/index.php
+++ PHPjob/3-2/index.php
@@ -1,14 +1,17 @@
 <?php
 $fruits = ["りんご" => "300", "みかん" => "150", "もも" => "3000"];
+$num = ["1","1","1"];
 
 function getprice($unit,$num){
     $price=$unit*$num;
     return $price;
 }
 
+$i=0;
 foreach($fruits as $key => $value){
-    echo $key."は".getprice($value,1)."円です。";
+    echo $key."は".getprice($value,$num[$i])."円です。";
     echo '<br>';
+    $i++;
 }
 
 ?>
